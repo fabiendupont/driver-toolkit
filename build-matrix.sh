8e5a7bba519504a3e1864f8c07a93acf11866739
Fabien Dupont <fabiendupont@pm.me>
2023-12-22 17:06:28 +0100

    Update job and matrix build script
    The build-matrix.sh script is now generating a _flat_ JSON output on a
    single line. The job definition has been updated to remove the pull
    secret.
    
    Signed-off-by: Fabien Dupont <fabiendupont@pm.me>

--- build-matrix.sh
+++ build-matrix.sh
@@ -2,16 +2,16 @@
 
 MATRIX_FILE="build-matrix.json"
 
-echo -e "{\n    \"versions\": [" > $MATRIX_FILE
+echo -n -e "{ \"versions\": [ " > $MATRIX_FILE
 
 # Get the list of kernels to build
 COUNT=0
 for i in $(docker run --rm quay.io/centos/centos:stream9 dnf repoquery kernel | cut -d ':' -f 2); do
     if [ $COUNT -gt 0 ]; then
-        echo -e "," >> $MATRIX_FILE
+        echo -n -e ", " >> $MATRIX_FILE
     fi
-    echo -n -e "        \"$i\"" >> $MATRIX_FILE
+    echo -n -e "\"$i\"" >> $MATRIX_FILE
     COUNT=$((COUNT+1))
 done
 
-echo -n -e "\n    ]\n}" >> $MATRIX_FILE
+echo -n -e " ] }" >> $MATRIX_FILE
